167ff74526b70d7674348cc67deb7de0f85c860d
James Ocana <jamesocana@macbook-air.myfiosgateway.com>
2022-09-11 17:52:08 -0400

    initial commit - serveer connected and mongodb connected

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const methodOverride = require("method-override");
+const PORT = process.env.PORT;
+const mongoose = require("mongoose");
+
+//Connect MongoDB
+mongoose.connect(process.env.DATABASE_URL);
+
+//MIDDLEWARE 
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+//database connection error / success
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err.message + "we got an error connecting the DB"));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
+
+app.listen(PORT, (req, res) => {
+    console.log("hello there!");
+});
